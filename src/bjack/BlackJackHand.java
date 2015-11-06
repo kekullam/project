0a235c010432186210fdcd0157d5c21812a5d789
kekullam <kerdo.kullamae@gmail.com>
2015-11-06 18:29:29 +0200

    extends hand.java

---
+++ src/bjack/BlackJackHand.java
@@ -0,0 +1,37 @@
+package bjack;
+
+/**
+ * Created by Kerdo Kullamäe on 6.11.2015.
+ */
+public class BlackJackHand extends Hand {
+
+
+    public int getBlackJackValue() {
+
+        int sum;
+        boolean ace;
+        int cards;
+
+        sum = 0;
+        ace = false;
+        cards = getCardCount();
+        for (int i = 0; i < cards; i++) {
+            Card card;
+            int cardVal;
+            card = getCard(i);
+            cardVal = card.getValue();
+            if (cardVal > 10) {
+                cardVal = 10;
+            }
+            if (cardVal == 1) {
+                ace = true;
+            }
+            sum = sum + cardVal;
+        }
+        if (ace = true && sum  < 10) {
+            sum = sum + 1;
+        }
+            return sum;
+        }
+    }
+
