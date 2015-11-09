bef7725c42ce1f8763e2fec24fc07c458749be61
kekullam <kerdo.kullamae@gmail.com>
2015-11-09 13:09:23 +0200

    tegin uue getBlackJackHand() meetodi

--- src/bjack/BlackJackHand.java
+++ src/bjack/BlackJackHand.java
@@ -8,31 +8,34 @@ public class BlackJackHand extends Hand {
 
     public int getBlackJackValue() {
 
-        int sum;
-        boolean ace;
-        int cards;
+        int sum = 0;
+        int aces = 0;
+        int cards = getCardCount();
 
-        sum = 0;
-        ace = false;
-        cards = getCardCount();
-        for (int i = 0; i < cards; i++) {
-            Card card;
-            int cardVal;
-            card = getCard(i);
-            cardVal = card.getValue();
-            if (cardVal > 10) {
-                cardVal = 10;
+            for (int i=0;i<cards;i++) {
+                switch(i) {
+                    case 1: sum += 1;
+                    case 2: sum += 2;
+                    case 3: sum += 3;
+                    case 4: sum += 4;
+                    case 5: sum += 5;
+                    case 6: sum += 6;
+                    case 7: sum += 7;
+                    case 8: sum += 8;
+                    case 9: sum += 9;
+                    case 10: sum += 10;
+                    case 11: sum += 10;
+                    case 12: sum += 10;
+                    case 13: sum += 10;
+                }
             }
-            if (cardVal == 1) {
-                ace = true;
+        for (int i=0; i < aces; i++) {
+            if (sum > 10) {
+                sum += 1;
+            } else {
+                sum += 11;
             }
-            sum = sum + cardVal;
         }
-        if (ace = true && sum + 10 <=21) {
-            sum = sum + 10;
-
-        }
-            return sum;
-        }
+        return sum;
     }
-
+}
